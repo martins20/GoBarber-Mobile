f5f8716c34cc061916e8a8275cff27e06a61cd09
martins20 <pdesignerpb@gmail.com>
2020-09-27 16:21:57 -0300

    Finish style of Register screen

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,9 +1,56 @@
 import React from 'react';
+import {
+  Image,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { logo } from '../../utils/imagesExports';
+
+import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
 
 const SignUp: React.FC = () => {
-  return <Container />;
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logo} />
+
+            <View>
+              <Title>Crie sua Conta</Title>
+            </View>
+
+            <Input name="name" icon="user" placeholder="Nome" />
+
+            <Input name="email" icon="mail" placeholder="E-mail" />
+
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button onPress={() => console.log('Foi')}>Entrar</Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <BackToSignIn onPress={() => {}}>
+        <Icon name="arrow-left" size={20} color="#fff" />
+        <BackToSignInText>Voltar para logon</BackToSignInText>
+      </BackToSignIn>
+    </>
+  );
 };
 
 export default SignUp;
