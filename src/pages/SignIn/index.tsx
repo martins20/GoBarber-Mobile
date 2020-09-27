f3a73b6408a56cf5611d30866dda03baa8b9fb47
martins20 <pdesignerpb@gmail.com>
2020-09-27 14:39:35 -0300

    Start create signin page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Image } from 'react-native';
+import { logo } from '../../utils/imgaesExports';
 
 import { Container } from './styles';
 
 const SignIn: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Image source={logo} />
+    </Container>
+  );
 };
 
 export default SignIn;
