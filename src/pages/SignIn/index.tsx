7a72765f941cd35805a5f10aca0a694ba3a4ccce
martins20 <pdesignerpb@gmail.com>
2020-09-27 14:51:26 -0300

    Add external font to project

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Image } from 'react-native';
 import { logo } from '../../utils/imgaesExports';
 
-import { Container } from './styles';
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logo} />
+
+      <Title>Faça seu Logon</Title>
     </Container>
   );
 };
